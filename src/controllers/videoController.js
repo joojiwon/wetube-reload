5ea859607063a6c71565294bf9e7e554794448b0
joojiwon <85204528+joojiwon@users.noreply.github.com>
2022-08-21 18:37:37 +0900

    red.body

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,7 @@ let videos = [
         rating:5,
         comments:2,
         createdAt: "2 minutes ago",
-        views:59,
+        views:1,
         id:1,
     },
     {
@@ -29,13 +29,35 @@ export const trending = (req, res) => {
     
     return res.render("home", {pageTitle: "Home", videos })
 };
-export const see = (req, res) => {
+export const watch = (req, res) => {
     const {id} = req.params;
     const video = videos[id-1]
-    return res.render("watch", {pageTitle: `Watching ${video.title}`})
+    return res.render("watch", {pageTitle: `Watching ${video.title}`, video})
 };
-export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
-
+export const getEdit = (req, res) => {
+    const {id} = req.params;
+    const video = videos[id-1]
+    return res.render("edit", {pageTitle: `Editing: ${video.title}`, video})
+};
+export const postEdit = (req, res) => {
+    const {id} = req.params;
+    const {title} = req.body;
+    videos[id-1].title = title;
+    return res.redirect(`/videos/${id}`);
+};
+export const getUpload = (req, res) => {
+    return res.render("upload", {pageTitle: "Upload Video"});
+};
+export const postUpload = (req, res) => {
+    const {title} = req.body;
+    const newVideo = {
+        title,
+        rating: 0,
+        comments: 0,
+        createdAt: "just now",
+        views: 0,
+        id: videos.length + 1,
+    };
+    videos.push(newVideo);
+    return res.redirect("/");
+};
