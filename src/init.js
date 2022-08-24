d4fa4ac8573c9f897d5677551002f9841aca8873
joojiwon <85204528+joojiwon@users.noreply.github.com>
2022-08-24 18:23:48 +0900

    callback basic

---
+++ src/init.js
@@ -0,0 +1,9 @@
+import "./db";
+import Video from "./models/Video";
+import app from "./server";
+
+const PORT = 4001;
+
+
+const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
+app.listen(PORT, handleListening);
--- src/server.js
+++ src/server.js
@@ -1,16 +1,13 @@
-import "./db";
+
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
-const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-
 
 
 app.set("view engine", "pug");
@@ -22,7 +19,6 @@ app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
 
+export default app;
 
 
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
-app.listen(PORT, handleListening);
