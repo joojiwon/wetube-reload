6fc1f6d57009fe76251567837625f266af66f6e0
joojiwon <85204528+joojiwon@users.noreply.github.com>
2022-08-28 20:03:41 +0900

    try, catch

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,7 @@ import Video from "../models/Video";
 
 export const home = async(req, res) => {
     const videos = await Video.find({})
-    return res.render("home", {pageTitle: "Home" , videos: []})
+    return res.render("home", {pageTitle: "Home" , videos})
 };
 export const watch = (req, res) => {
     const {id} = req.params;
@@ -22,7 +22,21 @@ export const postEdit = (req, res) => {
 export const getUpload = (req, res) => {
     return res.render("upload", {pageTitle: "Upload Video"});
 };
-export const postUpload = (req, res) => {
-    const {title} = req.body;
+export const postUpload = async (req, res) => {
+    const {title, description, hashtags} = req.body;
+    try {await Video.create({
+        title,
+        description,
+        hashtags: hashtags.split(",").map((word) => `#${word}`),
+        meta: {
+            views: 0,
+            rating: 0,
+        },
+        });  
     return res.redirect("/");
+} catch(error) {
+    return res.render("upload", {
+        pageTitle: "Upload Video", 
+        errorMessage: error._message,});
+}
 };
--- src/models/Video.js
+++ src/models/Video.js
@@ -3,13 +3,13 @@ import mongoose from "mongoose";
 const videoSchema = new mongoose.Schema({
     title: String, 
     description: String,
-    createdAt: Date,
-    hashtags: [{type: String}],
+    createdAt: { type: Date, required: true, default: Date.now },
+    hashtags: [{ type: String }],
     meta: {
-        views: Number,
-        rating: Number,
+        views: {type: Number, default: 0, required: true},
+        rating: {type: Number, default: 0, required: true},
 
-    }
+    },
 });
 
 const Video = mongoose.model("Video", videoSchema);
