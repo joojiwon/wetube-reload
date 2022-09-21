0cef52f569a07bfd6fca78efe7c53366397b0db6
joojiwon <85204528+joojiwon@users.noreply.github.com>
2022-09-21 21:46:56 +0900

    logout

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/usercontroller";
+import { edit, logout, see, startGithubLogin, finishGithubLogin } from "../controllers/usercontroller";
 
 const userRouter = express.Router();
 
 
-userRouter.get("/edit", edit);
-userRouter.get("/remove", remove);
-userRouter.get("/:id", see);
 userRouter.get("/logout", logout);
+userRouter.get("/edit", edit);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
+userRouter.get("/:id", see);
 
 export default userRouter;
